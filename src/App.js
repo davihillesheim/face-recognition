7a0409d8032e4164e29fe1b3c4bf8ac97be7ca5f
Davi Hillesheim <davi-241@hotmail.com>
2020-07-06 17:01:53 -0300

    add background pattern

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particles = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800,
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles
+        className='particles'  
+        params={particles}/>
       <Navigation />
       <Logo />
       <Rank />
