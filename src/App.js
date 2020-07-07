fc0e14258dfcbabf1b9fa1555e6fe6f1fc2c2eab
Davi Hillesheim <davi-241@hotmail.com>
2020-07-07 16:51:35 -0300

    change background color and minor refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -18,19 +18,32 @@ const particles = {
   }
 }
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  render() {
   return (
-    <div className="App">
-      <Particles
-        className='particles'  
-        params={particles}/>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+      <div className="App">
+        <Particles
+          className='particles'  
+          params={particles}/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
